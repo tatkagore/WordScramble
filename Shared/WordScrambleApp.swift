c727b6234fb3bc76b2c91da26b93d621e1d1f588
Tatiana Simmer <tatkagore@gmail.com>
2022-05-24 11:59:15 +0200

    Initial Commit

---
+++ Shared/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  Shared
+//
+//  Created by Tatiana Simmer on 24/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
